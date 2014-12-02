0d9e178abb7d462ec4491a22d3ae4b0b28136ce6
Jan Henckens <jan@onedge.be>
2014-12-02 23:00:50 +0100

    Adds link and title to widget

--- includes/class-wp-sponsors-widget.php
+++ includes/class-wp-sponsors-widget.php
@@ -12,14 +12,21 @@ class sponsors_widget extends WP_Widget {
     function widget($args, $instance) {
         extract( $args );
         $check_images = $instance['check_images'] ? 'true' : 'false';
+        // WP_Query arguments
+        $args = array (
+          'post_type'              => 'sponsor',
+          'post_status'            => 'publish',
+          'pagination'             => false,
+        );
 
-        ?>
-              <?php echo $before_widget; ?>
+        // The Query
+        $query = new WP_Query( $args ); ?>
+        <ul>
 
-              <ul>
-                <li><?php echo $check_images; ?></li>
-              </ul> 
-              <?php echo $after_widget; ?>
+          <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+              <li><a href="<?php echo get_post_meta( get_the_ID(), 'wp_sponsors_url', true ) ?>" target="_blank"><?php the_title(); ?></a></li>
+          <?php endwhile; wp_reset_postdata(); ?>
+          </ul>
         <?php
     }
 
