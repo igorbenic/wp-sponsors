b232e6c78961dcb9ca66110a8f18330130ceeaf8
Jan Henckens <jan@onedge.be>
2014-12-02 20:52:02 +0100

    Register a custom post type called "sponsors"

--- includes/class-wp-sponsors.php
+++ includes/class-wp-sponsors.php
@@ -29,188 +29,213 @@
  */
 class Wp_Sponsors {
 
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      Wp_Sponsors_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
-	protected $loader;
+  /**
+   * The loader that's responsible for maintaining and registering all hooks that power
+   * the plugin.
+   *
+   * @since    1.0.0
+   * @access   protected
+   * @var      Wp_Sponsors_Loader    $loader    Maintains and registers all hooks for the plugin.
+   */
+  protected $loader;
 
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $wp_sponsors    The string used to uniquely identify this plugin.
-	 */
-	protected $wp_sponsors;
+  /**
+   * The unique identifier of this plugin.
+   *
+   * @since    1.0.0
+   * @access   protected
+   * @var      string    $wp_sponsors    The string used to uniquely identify this plugin.
+   */
+  protected $wp_sponsors;
 
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
-	protected $version;
+  /**
+   * The current version of the plugin.
+   *
+   * @since    1.0.0
+   * @access   protected
+   * @var      string    $version    The current version of the plugin.
+   */
+  protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the Dashboard and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct() {
+  /**
+   * Define the core functionality of the plugin.
+   *
+   * Set the plugin name and the plugin version that can be used throughout the plugin.
+   * Load the dependencies, define the locale, and set the hooks for the Dashboard and
+   * the public-facing side of the site.
+   *
+   * @since    1.0.0
+   */
+  public function __construct() {
 
-		$this->wp_sponsors = 'wp-sponsors';
-		$this->version = '1.0.0';
+    $this->wp_sponsors = 'wp-sponsors';
+    $this->version = '1.0.0';
 
-		$this->load_dependencies();
-		$this->set_locale();
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
+    $this->load_dependencies();
+    $this->set_locale();
+    $this->define_admin_hooks();
+    $this->define_public_hooks();
 
-	}
+  }
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Wp_Sponsors_Loader. Orchestrates the hooks of the plugin.
-	 * - Wp_Sponsors_i18n. Defines internationalization functionality.
-	 * - Wp_Sponsors_Admin. Defines all hooks for the dashboard.
-	 * - Wp_Sponsors_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function load_dependencies() {
+  /**
+   * Load the required dependencies for this plugin.
+   *
+   * Include the following files that make up the plugin:
+   *
+   * - Wp_Sponsors_Loader. Orchestrates the hooks of the plugin.
+   * - Wp_Sponsors_i18n. Defines internationalization functionality.
+   * - Wp_Sponsors_Admin. Defines all hooks for the dashboard.
+   * - Wp_Sponsors_Public. Defines all hooks for the public side of the site.
+   *
+   * Create an instance of the loader which will be used to register the hooks
+   * with WordPress.
+   *
+   * @since    1.0.0
+   * @access   private
+   */
+  private function load_dependencies() {
 
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-sponsors-loader.php';
+    /**
+     * The class responsible for orchestrating the actions and filters of the
+     * core plugin.
+     */
+    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-sponsors-loader.php';
 
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-sponsors-i18n.php';
+    /**
+     * The class responsible for defining internationalization functionality
+     * of the plugin.
+     */
+    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-sponsors-i18n.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the Dashboard.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wp-sponsors-admin.php';
+    /**
+     * The class responsible for defining all actions that occur in the Dashboard.
+     */
+    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wp-sponsors-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-sponsors-public.php';
+    /**
+     * The class responsible for defining all actions that occur in the public-facing
+     * side of the site.
+     */
+    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-sponsors-public.php';
 
-		$this->loader = new Wp_Sponsors_Loader();
+    $this->loader = new Wp_Sponsors_Loader();
 
-	}
+  }
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Wp_Sponsors_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function set_locale() {
+  /**
+   * Define the locale for this plugin for internationalization.
+   *
+   * Uses the Wp_Sponsors_i18n class in order to set the domain and to register the hook
+   * with WordPress.
+   *
+   * @since    1.0.0
+   * @access   private
+   */
+  private function set_locale() {
 
-		$plugin_i18n = new Wp_Sponsors_i18n();
-		$plugin_i18n->set_domain( $this->get_wp_sponsors() );
+    $plugin_i18n = new Wp_Sponsors_i18n();
+    $plugin_i18n->set_domain( $this->get_wp_sponsors() );
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+    $this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
-	}
+  }
 
-	/**
-	 * Register all of the hooks related to the dashboard functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_admin_hooks() {
+  /**
+   * Register all of the hooks related to the dashboard functionality
+   * of the plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   */
+  private function define_admin_hooks() {
 
-		$plugin_admin = new Wp_Sponsors_Admin( $this->get_wp_sponsors(), $this->get_version() );
+    $plugin_admin = new Wp_Sponsors_Admin( $this->get_wp_sponsors(), $this->get_version() );
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+    $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
+    $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
-	}
+  }
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
+  /**
+   * Register all of the hooks related to the public-facing functionality
+   * of the plugin.
+   *
+   * @since    1.0.0
+   * @access   private
+   */
+  private function define_public_hooks() {
 
-		$plugin_public = new Wp_Sponsors_Public( $this->get_wp_sponsors(), $this->get_version() );
+    $plugin_public = new Wp_Sponsors_Public( $this->get_wp_sponsors(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+    $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+    $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
-	}
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
-	public function run() {
-		$this->loader->run();
-	}
+  }
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_wp_sponsors() {
-		return $this->wp_sponsors;
-	}
+  /**
+   * Run the loader to execute all of the hooks with WordPress.
+   *
+   * @since    1.0.0
+   */
+  public function run() {
+    $this->loader->run();
+    function sponsors_register() {
+            $args = array(
+              'public'              => true,
+              'label'               => 'Sponsors',
+              'public'              => true,
+              'exclude_from_search' => true,
+              'publicly_queryable'  => false,
+              'show_ui'             => true,
+              'show_in_menu'        => true,
+              'show_in_admin_bar'   => false,
+              'menu_position'       => 5,
+              'menu_icon'           => 'dashicons-format-image',
+              'query_var'           => true,
+              'rewrite'             => false,
+              'capability_type'     => 'post',
+              'has_archive'         => false,
+              'hierarchical'        => false,
+              'can_export'          => true,
+              'query_var'           => false,
+              'supports'            => array( 'title' )
+              );
+        register_post_type( 'sponsor', $args );
+        }
+        add_action( 'init', 'sponsors_register' );
+  }
 
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    Wp_Sponsors_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
-		return $this->loader;
-	}
+  /**
+   * The name of the plugin used to uniquely identify it within the context of
+   * WordPress and to define internationalization functionality.
+   *
+   * @since     1.0.0
+   * @return    string    The name of the plugin.
+   */
+  public function get_wp_sponsors() {
+    return $this->wp_sponsors;
+  }
 
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
+  /**
+   * The reference to the class that orchestrates the hooks with the plugin.
+   *
+   * @since     1.0.0
+   * @return    Wp_Sponsors_Loader    Orchestrates the hooks of the plugin.
+   */
+  public function get_loader() {
+    return $this->loader;
+  }
+
+  /**
+   * Retrieve the version number of the plugin.
+   *
+   * @since     1.0.0
+   * @return    string    The version number of the plugin.
+   */
+  public function get_version() {
+    return $this->version;
+  }
 
 }
