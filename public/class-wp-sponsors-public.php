917005c4e63737b1f83cf1ebbd15e44ec9cbdb2b
Jan Henckens <jan@onedge.be>
2016-06-12 20:49:29 +0200

    Don't queue js when we don't need it

--- public/class-wp-sponsors-public.php
+++ public/class-wp-sponsors-public.php
@@ -96,7 +96,7 @@ class Wp_Sponsors_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->wp_sponsors, plugin_dir_url( __FILE__ ) . 'js/wp-sponsors-public.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->wp_sponsors, plugin_dir_url( __FILE__ ) . 'js/wp-sponsors-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
