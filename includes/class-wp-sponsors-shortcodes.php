c54b86d88c1ce6b2160e650d8b94945376f09457
igor <i.benic@hotmail.com>
2021-05-14 23:31:54 +0200

    3.4.0

--- includes/class-wp-sponsors-shortcodes.php
+++ includes/class-wp-sponsors-shortcodes.php
@@ -264,8 +264,7 @@ class WP_Sponsors_Shortcodes {
 						)
 					),
                 );*/
-				$style['containerPre'] = '<div id="wp-sponsors" class="clearfix slider wp-sponsors ' . $atts['slider_image'] . '" data-slick="' . esc_attr( wp_json_encode( $slickSettings ) ) . '">';
-				$style['containerPre'] = '<div id="wp-sponsors" class="clearfix slider wp-sponsors ' . ( 1 === absint( $atts['verticalcenter'] ) ? 'vertical-center' : '' ) . '" data-slick="' . esc_attr( wp_json_encode( $slickSettings ) ) . '">';
+				$style['containerPre'] = '<div id="wp-sponsors" class="clearfix slider wp-sponsors ' . $atts['slider_image'] . ' ' . ( 1 === absint( $atts['verticalcenter'] ) ? 'vertical-center' : '' ) . '" data-slick="' . esc_attr( wp_json_encode( $slickSettings ) ) . '">';
 				$style['containerPost'] = '</div>';
 				$style['wrapperClass'] = 'sponsor-item';
 				$style['wrapperPre'] = 'div';
